09bbeae22c0828f560518761d06acdcfcbc59e25
Kolchanov.VI <Kolchanov.VI@parma.ru>
2022-07-03 11:53:39 +0500

    Create submodule 'backend'

--- backend/src/main/java/ru/itresource/ItResourceApplication.java
+++ backend/src/main/java/ru/itresource/ItResourceApplication.java
@@ -3,7 +3,6 @@ package ru.itresource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Slf4j
