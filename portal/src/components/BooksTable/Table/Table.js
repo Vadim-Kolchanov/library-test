ae3c28a00b1373262818785954281f7f2a052015
Kolchanov.VI <Kolchanov.VI@parma.ru>
2022-07-06 00:01:02 +0500

    improved logic with limit

--- portal/src/components/BooksTable/Table/Table.js
+++ portal/src/components/BooksTable/Table/Table.js
@@ -6,6 +6,8 @@ import {LibraryContext} from "../../../context/library/LibraryContext";
 
 const HEADERS = ['#', 'Название книги', 'Автор', 'Дата выпуска', ''];
 
+const FIRST_PAGE = 0;
+
 export const Table = ({catalog}) => {
     const {undoBookCreation, getBooks, pageable, books, addNewBook, setPage, setLimit} = useContext(LibraryContext);
     const [isAdding, setIsAdding] = useState(false);
@@ -27,10 +29,10 @@ export const Table = ({catalog}) => {
     const header = <Header {...{catalog, isAdding, setIsAdding, addNewBook}}/>;
 
     if (books.length === 0) {
-        if (pageable.totalPages > 1) {
-            const currentPage = pageable.page;
+        if (pageable.totalElements > 0) {
+            const currentPage = pageable.limit >= pageable.totalElements ? FIRST_PAGE : pageable.page;
 
-            getBooks(catalog.id, {...pageable, page: currentPage === 0 ? currentPage : currentPage - 1});
+            getBooks(catalog.id, {...pageable, page: currentPage === FIRST_PAGE ? currentPage : currentPage - 1});
             return;
         }
 
